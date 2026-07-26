532939208a37cadc47e7f264ab8251032996b243
Abir Talukder <166286745+abx667@users.noreply.github.com>
2026-07-26 15:02:05 +0530

    Add files via upload

---
+++ gatewayapp/src/main/java/com/cognizant/trade/GatewayappApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayappApplication.class, args);
+	}
+
+}

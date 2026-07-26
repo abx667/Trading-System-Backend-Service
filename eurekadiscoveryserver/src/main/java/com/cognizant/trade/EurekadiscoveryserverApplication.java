682eef559eace26d8d6fa4c6c1cd2fddbf65e81d
Abir Talukder <166286745+abx667@users.noreply.github.com>
2026-07-26 15:00:52 +0530

    Add files via upload

---
+++ eurekadiscoveryserver/src/main/java/com/cognizant/trade/EurekadiscoveryserverApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekadiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekadiscoveryserverApplication.class, args);
+	}
+
+}
